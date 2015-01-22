9495a1e9c8cb23450428cd19c0bd5a95da6eb17c
Vladimir Sudilovsky <vsudilovsky@gmail.com>
2015-01-22 14:54:37 +0000

    config: allow setting of config via kwargs passed to Discoverer.__init__() or discoverer.init_app()

--- flask_discoverer.py
+++ flask_discoverer.py
@@ -1,6 +1,11 @@
 from flask import current_app
 import json
 
+DEFAULT_CONFIG = {
+  'DISCOVERER_PUBLISH_ENDPOINT':'/resources',
+  'DISCOVERER_SELF_PUBLISH':False,
+}
+
 class Discoverer(object):
   '''
   API endpoint autodiscovery
@@ -8,28 +13,34 @@ class Discoverer(object):
 
   def __init__(self, app=None, **kwargs):
     self.app = app
+    self.kwargs = kwargs if kwargs else {}
     if app is not None:
-      self.init_app(app, **kwargs)
+      self.init_app(app)
 
-  def init_app(self, app, find_resources=True, **kwargs):
+  def init_app(self, app, **kwargs):
     self.app = app
-    self.resources = {}
+    self.kwargs.update(kwargs)
     if not hasattr(app, 'extensions'):
       app.extensions = {}
     if 'discoverer' in app.extensions:
       raise RuntimeError("Flask application already initialized")
     app.extensions['discoverer'] = self
     config = app.config
-    config.setdefault('DISCOVERER_PUBLISH_ENDPOINT','/resources')
-    if find_resources:
-      with self.app.app_context():
-        self.find_resources()
 
-  def find_resources(self,force=False):
-    if self.resources and not force:
-      raise RuntimeError("Resources were already discovered. Re-discover with force=True")
+    for k,v in DEFAULT_CONFIG.iteritems():
+      config.setdefault(k,v)
+      if k in self.kwargs:
+        config.update({k:self.kwargs[k]})
+
+    route = config['DISCOVERER_PUBLISH_ENDPOINT']
+    with self.app.app_context():
+      current_app.add_url_rule(route,route,lambda: self.find_resources(config['DISCOVERER_SELF_PUBLISH']))
+
+  def find_resources(self,self_publish=False):
     resources = {}
     for rule in current_app.url_map.iter_rules():
+      if not self_publish and rule.rule == current_app.config['DISCOVERER_PUBLISH_ENDPOINT']:
+        continue
       resources[rule.rule] = {}
       f = current_app.view_functions[rule.endpoint]
       advertised = f._advertised if hasattr(f,'_advertised') else []
@@ -40,8 +51,7 @@ class Discoverer(object):
           resources[rule.rule].update({key:f.view_class.__getattribute__(f.view_class,key)})
         if hasattr(f,key):
           resources[rule.rule].update({key:f.__getattribute__(key)})
-    route = current_app.config['DISCOVERER_PUBLISH_ENDPOINT']
-    current_app.add_url_rule(route,route,lambda: json.dumps(resources))
+    return json.dumps(resources)
 
 def advertise(*args,**kwargs):
   def decorator(f):
